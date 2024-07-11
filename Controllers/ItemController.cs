c892accc03d21511fdaea824f4765ca8d19f3ee7
Jobayrul Hasan <jobayerdpi11@gmail.com>
2024-07-11 17:09:01 +0600

    second changes

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -1,4 +1,5 @@
 ﻿using Core_pro_with_react.Models;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_pro_with_react.Controllers
@@ -7,7 +8,7 @@ namespace Core_pro_with_react.Controllers
     [Route("[controller]")]
     public class ItemController : ControllerBase
     {
-        private static readonly IEnumerable<ItemModel> itemModels = new[]
+        private static readonly IEnumerable<ItemModel> Items = new[]
         {
             new ItemModel { Id = 1, Title = "The GodFather", ImageId = 1, Ranking = 0, TtemType = 1},
             new ItemModel { Id = 2, Title = "Highlander", ImageId = 2, Ranking = 0, TtemType = 1},
@@ -20,5 +21,12 @@ namespace Core_pro_with_react.Controllers
             new ItemModel { Id = 1, Title = "Star Treck IV", ImageId = 9, Ranking = 0, TtemType = 1},
             new ItemModel { Id = 1, Title = "Superman 4", ImageId = 10, Ranking = 0, TtemType = 1},
         };
+
+        [HttpGet("{itemType:int}")] //specify the item typ
+        public ItemModel[] Get(int itemType) //depending on parameter we can get data here
+        {
+            ItemModel[] items = Items.Where(i => i.TtemType == itemType).ToArray();
+            return items;
+        }
     }
 }
